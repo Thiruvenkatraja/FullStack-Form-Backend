9b4ec5d70a6a120a594e9b1a5b21007ee7a53653
Thiruvenkatraja <venkat.tv.raja@gmail.com>
2022-12-20 22:23:57 +0530

    cors update

--- Server.js
+++ Server.js
@@ -25,9 +25,7 @@ mongoose.connect('mongodb+srv://venkat999:venkat%40999@cluster0.4zuppce.mongodb.
       extended:false
   }))
 
-  app.use(cors({
-    origin:["http://localhost:3000","https://enq-form-frontend.onrender.com/","https://full-stack-form-frontent.vercel.app/"]
-  }))
+  app.use(cors())
   
   app.use('/enq',enquirySchema)
 
